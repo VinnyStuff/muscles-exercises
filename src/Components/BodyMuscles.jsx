4d3dfc01b2529f490aa90eaadbcfec4bbc1f3e41
VinnyStuffs <70545559+VinnyStuffs@users.noreply.github.com>
2023-03-17 14:06:45 -0300

    removing useless class

--- src/Components/BodyMuscles.jsx
+++ src/Components/BodyMuscles.jsx
@@ -7,12 +7,11 @@ import '../ComponentsStyles/BodyMuscles.css'
 function BodyMuscles() {
     const [currentMuscle, setCurrentMuscle] = useState('');
     const muscleActiveColor = 'red';
+
   
     function handleClick(event) {
-      const clickedMuscleClass = event.target.classList.item(1);
+      const clickedMuscleClass = event.target.classList.value;
   
-        console.log(clickedMuscleClass);
-
       if (clickedMuscleClass) {
         setCurrentMuscle(clickedMuscleClass);
       } else {
@@ -20,22 +19,8 @@ function BodyMuscles() {
       }
     }
 
-    const [bla, setBla] = useState('');
-    function bla1(){
-        setBla('muscle-map-container')
-    }
-    function bla2(){
-        setBla('')
-    }
-  
     return (
       <div>
-        <button onClick={bla1}>
-            button 1
-        </button>
-        <button onClick={bla2}>
-            button 2
-        </button>
         <div className="muscle-map-container muscle-map">
           <MuscleMap />
         </div>
@@ -43,13 +28,9 @@ function BodyMuscles() {
           <MuscleMapEnable />
         </div>
         <style>
-          {`.muscle.${currentMuscle} {
+          {`.${currentMuscle} {
             cursor: pointer;
             fill: ${muscleActiveColor};
-          }`}
-
-          {`.${bla} {
-            background-color:red;
           }`}
         </style>
       </div>
